9acbac4f69b04b7c6a4433a6c5de80dd421a5100
Maxim <maks.bidenko.96@mail.ru>
2022-12-05 10:57:18 +0300

    Сделал 1 задание

---
+++ ex01.java
@@ -0,0 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+
+
+
+public class ex01 {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkList = linkedList();
+        revers(linkList);
+    }
+    public static LinkedList<Integer> revers(LinkedList<Integer> lst){
+        ArrayDeque<Integer> arr = new ArrayDeque<>();
+        for (int i = 0; i < lst.size(); i++) {
+            arr.add(lst.get(i));
+
+        }
+        while (lst.size()>0) {
+            lst.removeFirst();
+        }
+        while (arr.size()>0) {
+            lst.add(arr.getLast());
+            arr.remove(arr.getLast());
+        } 
+        System.out.println(lst);
+        return lst;
+        
+    }
+    public static LinkedList<Integer> linkedList() {
+        LinkedList<Integer> lst = new LinkedList<>();
+            lst.addLast(1);
+            lst.addLast(2);
+            lst.addLast(3);
+            lst.addLast(4);
+            System.out.println(lst);
+           return lst; 
+        }
+    }
+    
+
